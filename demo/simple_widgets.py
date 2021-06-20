f4e54483a1c771543d9277abbd0e9b72b632ed99
Chaddaï Fouché <chaddai.fouche@gmail.com>
2021-06-20 20:05:11 +0200

    Small demo for the buttons

---
+++ demo/simple_widgets.py
@@ -0,0 +1,55 @@
+import pygame
+from pygame.constants import *
+import wipyg
+from wipyg import buttons
+from wipyg.abstracts import Button
+
+pygame.init()
+
+screen = pygame.display.set_mode((320, 240))
+screen_rect = screen.get_rect()
+
+but_quit = buttons.StandardButton(text="Quitter")
+
+
+def quit(s, e):
+    global looping
+    if e.button == s:
+        looping = False
+
+
+but_quit.add_reaction(Button.CLICKED, quit)
+but_quit.rect.bottomright = screen_rect.bottomright
+
+
+but_print = buttons.StandardButton(text="Hello")
+
+
+def hello(s, e):
+    if e.button == s:
+        print("hello world !")
+
+
+but_print.add_reaction(Button.CLICKED, hello)
+
+but_disabled = buttons.StandardButton()
+but_disabled.state = Button.DISABLED
+but_disabled.rect.bottomleft = screen_rect.bottomleft
+
+widgets = pygame.sprite.RenderPlain(but_quit, but_print, but_disabled)
+
+looping = True
+while looping:
+    for e in pygame.event.get():
+        for w in widgets:
+            w.react(e)
+        if e.type == QUIT:
+            looping = False
+
+    widgets.update()
+    screen.fill((255, 255, 255))
+    widgets.draw(screen)
+
+    pygame.display.flip()
+
+pygame.quit()
